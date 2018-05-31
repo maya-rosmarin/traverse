5ce04f2dd9bc195aeb74e4591bec7b69455e4e72
Maya Rosmarin <maya.rosmarin@gmail.com>
2018-05-31 13:44:47 -0400

    Start work on Prims

--- maze_generators/prims.js
+++ maze_generators/prims.js
@@ -1,6 +1,43 @@
+import { createGridArray } from './create_grid';
+
 export default class Prims () {
   constructor (width, height) {
     this.grid = createGridArray(width, height);
+    this.width = width;
+    this.height = height;
+    this.frontier = [];
+  }
+
+  mark (xCoord, yCoord) {
+    this.findCellByLocation(xCoord, yCoord)[2] = 'in'
+    this.addFrontier(xCoord-2, yCoord)
+    this.addFrontier(xCoord+2, yCoord)
+    this.addFrontier(xCoord-2, yCoord)
+    this.addFrontier(xCoord+2, yCoord)
+  }
+
+  addFrontier(xCoord, yCoord) {
+    if (xCoord >= 0 && yCoord >= 0 && xCoord < this.width && yCoord < this.height) {
+      this.findCellByLocation(xCoord, yCoord)[2] = 'frontier';
+      this.frontier.push([xCoord, yCoord]);
+    }
+  }
+
+  neighbors (xCoord, yCoord) {
+    let neighbors = [];
+    if (xCoord > 0 && this.findCellByLocation(yCoord, xCoord - 2)) {
+      neighbors.push(this.findCellByLocation(xCoord - 2, yCoord))
+    }
+    if (xCoord + 2 < this.height && this.findCellByLocation(yCoord, xCoord + 2)) {
+      neighbors.push(this.findCellByLocation(xCoord + 2, yCoord))
+    }
+    if (yCoord > 0 && this.findCellByLocation(yCoord - 2, xCoord)) {
+      neighbors.push(this.findCellByLocation(xCoord, yCoord - 2))
+    }
+  }
+
+  findCellByLocation (xCoord, yCoord) {
+    return this.grid.find(cell => cell[0] === xCoord && cell[1] === yCoord);
   }
 
   animate () {
